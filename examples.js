fcd079bc2fecac9a2a0c3211c7240a50b0eea732
Kostas Karvounis <kael89@users.noreply.github.com>
2020-09-13 23:49:43 +1000

    Add examples (#2)

---
+++ examples.js
@@ -0,0 +1,60 @@
+/**
+ * Errors
+ */
+
+// no-unused-vars
+let a;
+// Disable no-unused-vars since it comes in handy in examples
+// This is done after we checked that it correctly shows an error
+/* eslint-disable no-unused-vars */
+
+// no-restricted-syntax: ForInStatement
+for (const key in {}) {
+}
+
+// react/jsx-filename-extension
+const CheckExtension = () => <h1></h1>;
+
+/**
+ * Warnings
+ */
+
+/* eslint-disable import/first */
+import React from 'react';
+import Jest from 'jest';
+/* eslint-enable import/first */
+
+/**
+ * Correct
+ */
+
+// import/prefer-default-export
+export const func = () => {};
+
+// no-continue
+// no-plusplus
+for (let i = 0; i < 1; i++) {
+  if (i > 1) {
+    continue;
+  }
+}
+
+// no-restricted-globals
+isNaN(1);
+isFinite(1);
+
+// no-restricted-syntax
+let x;
+for (const item of [1, 2]) {
+  x = item;
+}
+
+// no-use-before-define
+const main = () => dependency();
+const dependency = () => {};
+
+// radix
+const int = parseInt(10);
+
+// react/jsx-props-no-spreading
+const AllowPropsSpreading = props => <a {...props}>link</a>;
